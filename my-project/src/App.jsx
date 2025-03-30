132baf420455b158e225725ce8412a3d3ce93a45
monynymo <rmony4588@gmail.com>
2025-03-30 17:52:12 +0700

    some changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -10,7 +10,6 @@ import PaymentPage from "./Components/Navbar/PaymentPage"; // Payment Page
 import AboutUs from "./Components/Navbar/AboutUs"; // About Us Page
 import Dashboard from "./Components/Navbar/Dashboard"; // Dashboard Page
 import Login from "./Components/Auth/Login"; // Login Page
-import Register from "./Components/Auth/Register"; // Register Page
 
 const Layout = () => {
   const location = useLocation();
